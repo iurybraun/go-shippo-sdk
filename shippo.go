dc01fc4820b75c383dc337385f1f7d9446b8dd6e
Iury Braun <32597667+iurybraun@users.noreply.github.com>
2024-06-24 18:42:21 -0300

    Add files via upload

---
+++ shippo.go
@@ -0,0 +1,19 @@
+package shippo
+
+import "github.com/iurybraun/go-shippo-sdk/client"
+
+const (
+	APIVersion20140211 = "2014-02-11"
+	APIVesrion20161025 = "2016-10-25"
+	APIVersion20170329 = "2017-03-29"
+)
+
+// NewClient creates a new Shippo client.
+func NewClient(privateToken string) *client.Client {
+	return client.NewClient(privateToken, "")
+}
+
+// NewClientWithVersion creates a new Shippo client with API version explicitly specified.
+func NewClientWithVersion(privateToken, apiVersion string) *client.Client {
+	return client.NewClient(privateToken, apiVersion)
+}
